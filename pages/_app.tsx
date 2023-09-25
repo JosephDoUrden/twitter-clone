b35351205af0b4faf5e10690e4299d07776c05f0
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-26 00:41:12 +0300

    Login and Register Modal etc.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -6,6 +8,8 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <RegisterModal />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
