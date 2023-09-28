7e5355189458e01bd22a7f58b72f837e35642f3d
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-28 19:20:41 +0300

    currentUser quick bugfix

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,17 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "@/libs/serverAuth";
 
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  if(req.method !== "GET") {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
     return res.status(500).json({ message: "Only GET requests allowed" }); // 500 Internal Server Error
   }
 
   try {
-    const {currentUser} = await serverAuth(req);
+    const { currentUser } = await serverAuth(req);
 
-    return res.status(200).json({ currentUser }); // 200 OK
+    return res.status(200).json(currentUser); // 200 OK
   } catch (error) {
     console.log(error);
     return res.status(400).json({ message: "Something went wrong" }); // 400 Bad Request
