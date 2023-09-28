06cc49626d7c68d5bc8a8513897f6f64e20dc368
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-28 15:46:34 +0300

    UserHero UserBio etc.

---
+++ components/users/UserBio.tsx
@@ -0,0 +1,15 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useUser from "@/hooks/useUser";
+
+interface UserBioProps {
+  userId: string;
+}
+
+const UserBio: React.FC<UserBioProps> = ({ userId }) => {
+  const { data: currentUser } = useCurrentUser();
+  const { data: fetchedUser } = useUser(userId);
+
+  return <div></div>;
+};
+
+export default UserBio;
---
+++ components/users/UserHero.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import useUser from "@/hooks/useUser";
+
+import Avatar from "../Avatar";
+
+interface UserHeroProps {
+  userId: string;
+}
+
+const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+  const { data: fetchedUser } = useUser(userId);
+
+  return (
+    <div className="bg-neutral-700 h-44 relative">
+      {fetchedUser?.coverImage && (
+        <Image
+          src={fetchedUser.coverImage}
+          alt="cover-image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      )}
+      <div className="absolute -bottom-16 left-4">
+        <Avatar userId={userId} isLarge hasBorder />
+      </div>
+    </div>
+  );
+};
+
+export default UserHero;
