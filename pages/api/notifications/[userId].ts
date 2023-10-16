3ff0e8b54cabf61d2e7bfd8c08b4fc22635720c3
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-10-16 22:12:33 +0300

    Notifications hook done

---
+++ pages/api/notifications/[userId].ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@/libs/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" }); // 405 Method not allowed
+  }
+
+  try {
+    const { userId } = req.query;
+
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid ID");
+    }
+
+    const notifications = await prisma.notification.findMany({
+      where: { userId },
+      orderBy: { createdAt: "desc" },
+    });
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: { hasNotification: false },
+    });
+
+    return res.status(200).json(notifications); // 200 OK
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" }); // 500 Internal server error
+  }
+}
