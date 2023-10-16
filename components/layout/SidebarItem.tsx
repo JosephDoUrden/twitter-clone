54abdee095bc52b48db2a5c6e005815c0acc5b9c
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-10-16 21:29:40 +0300

    Notification icon done

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
+import { BsDot } from "react-icons/bs";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
@@ -11,6 +12,7 @@ interface SidebarItemProps {
   icon: IconType;
   onClick?: () => void;
   auth?: boolean;
+  alert?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
@@ -19,6 +21,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
   icon: Icon,
   onClick,
   auth,
+  alert,
 }) => {
   const loginModal = useLoginModal();
   const { data: currentUser } = useCurrentUser();
@@ -37,10 +40,16 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     <div onClick={handleClick} className="flex flex-row items-center">
       <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-opacity-10 hover:bg-slate-300 cursor-pointer transition lg:hidden">
         <Icon size={24} color="white" />
+        {alert ? (
+          <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} />
+        ) : null}
       </div>
       <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer">
         <Icon size={24} color="white" />
         <p className="hidden lg:block text-white text-xl">{label}</p>
+        {alert ? (
+          <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} />
+        ) : null}
       </div>
     </div>
   );
